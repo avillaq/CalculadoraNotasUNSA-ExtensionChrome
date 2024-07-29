a0a906986c7d18d4b5e55ad6c60782abc4cddbdd
avillaq <villafuertequispealex@gmail.com>
2024-07-29 18:16:54 -0500

    Inputs para ingresar notas

--- js/calculadora.js
+++ js/calculadora.js
@@ -5,6 +5,34 @@ function init(){
     const curso = urlParams.get('curso');
     document.getElementById('curso').innerText = curso;
 
+    chrome.storage.session.get(['notas']).then(function (result) {
+        //let listaCursos = document.querySelector("#lista-cursos");
+        let notas = result.notas.filter(nota => nota.curso === curso);
+        console.log(notas);
+        return
+        if (notas) {
+            let setCursos = new Set();
+            notas.forEach(nota => {
+                setCursos.add(nota.curso);
+            });
+            let cursos = Array.from(setCursos);
+            let content = "";
+            cursos.forEach(curso => {
+                content += `<button class="list-item" id="curso">${curso}</button>`;
+            });
+            listaCursos.innerHTML = content;
+        }
 
+        const btnCursos = document.querySelectorAll("#curso");
+        btnCursos.forEach(btnCurso => {
+            btnCurso.addEventListener('click', function (e) {
+                e.preventDefault();
+                let curso = btnCurso.textContent;
+                window.location.href = 'popup2.html?curso=' + encodeURIComponent(curso);
+            }
+            );
+        }
+        );
+    });
 
 }
