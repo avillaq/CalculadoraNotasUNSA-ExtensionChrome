485790ca3ed3531720cb46c2f719ba20e4a15664
avillaq <villafuertequispealex@gmail.com>
2024-07-29 19:47:50 -0500

    Calculadora de notas funcional

--- js/calculadora.js
+++ js/calculadora.js
@@ -29,6 +29,29 @@ function init(){
         );
     });
 
+    document.querySelector("#calcular").addEventListener('click', function (e) {
+        e.preventDefault();
+        let notaFinal = 0.0;
+        for (let i = 1; i <= 3; i++) {
+            let notaContinua = document.querySelector(`#continua${i}`).value;
+            let pesoContinua = document.querySelector(`#peso-continua${i}`).value;
+
+            let notaExamen = document.querySelector(`#examen${i}`).value;
+            let pesoExamen = document.querySelector(`#peso-examen${i}`).value;
+
+            if (notaContinua && pesoContinua) {
+                notaFinal += (parseFloat(notaContinua) * parseFloat(pesoContinua)) / 100.0;
+            }
+            if (notaExamen && pesoExamen) {
+                notaFinal += (parseFloat(notaExamen) * parseFloat(pesoExamen)) / 100.0;
+            }
+        }
+        // Redondear el resultado final a dos decimales
+        notaFinal = notaFinal.toFixed(2);
+        document.querySelector("#nota-final").innerHTML = `NOTA FINAL: ${notaFinal}`;
+
+    });
+
 
     function getTipoIndentificador(nota) {
         let parcial = nota.parcial;
